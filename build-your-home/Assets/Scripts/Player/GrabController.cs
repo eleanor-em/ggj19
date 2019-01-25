a225f698789d6b100982975b7458ddac1a755028
Jimbles <jim_bob_smith@live.com.au>
2019-01-26 01:42:35 +1100

    Fixed bounding bug and snapped grabbed objects to grid

--- build-your-home/Assets/Scripts/Player/GrabController.cs
+++ build-your-home/Assets/Scripts/Player/GrabController.cs
@@ -2,37 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrabController : MonoBehaviour {
+public class GrabController : MonoBehaviour
+{
     public LayerMask grabbableLayer;
 
     private MoveController moveController;
     private GrabbableController target = null;
     private bool holding = false;
 
-    private void Start() {
+    private void Start()
+    {
         moveController = GetComponent<MoveController>();
     }
 
-    void Update () {
+    void Update()
+    {
         CheckForGrabbable();
         CheckForGrabAction();
         UpdateHeld();
-	}
+    }
 
-    private void CheckForGrabbable() {
-        if (!holding) {
+    private void CheckForGrabbable()
+    {
+        if (!holding)
+        {
             // check the facing direction for a grabbable object
             var hit = Physics2D.Raycast(transform.position, moveController.Direction, 1, grabbableLayer.value);
-            if (hit.collider != null) {
+            if (hit.collider != null)
+            {
                 // if we hit something, check if it's both a grabbable and a different target
                 var hitController = hit.collider.GetComponent<GrabbableController>();
-                if (hitController?.gameObject != target?.gameObject) {
+                if (hitController?.gameObject != target?.gameObject)
+                {
                     target?.OnDeselect();
                     target = hitController;
                     target.OnSelect();
                 }
-            } else {
-                if (target != null) {
+            }
+            else
+            {
+                if (target != null)
+                {
                     target.OnDeselect();
                     target = null;
                 }
@@ -40,22 +50,47 @@ public class GrabController : MonoBehaviour {
         }
     }
 
-    private void CheckForGrabAction() {
-        if (Input.GetButtonDown("Pickup") && target != null) {
-            if (!holding) {
+    private void CheckForGrabAction()
+    {
+        if (Input.GetButtonDown("Pickup") && target != null)
+        {
+            if (!holding)
+            {
                 holding = true;
                 target.OnGrab();
-            } else {
+            }
+            else
+            {
                 holding = false;
                 target.OnDrop();
             }
         }
     }
 
-    private void UpdateHeld() {
-        if (holding) {
-            // TODO: make object snap to grid
-            target.transform.position = transform.position + moveController.Direction;
+    private void UpdateHeld()
+    {
+        if (holding)
+        {
+            Vector3 newPosition = SnapToPoint5(transform.position + moveController.Direction);
+
+            target.transform.position = newPosition;
         }
+    }
+
+    Vector3 SnapToPoint5(Vector3 input)
+    {
+        return new Vector3((float)(Mathf.Floor(input.x) + 0.5), (float)(Mathf.Floor(input.y) + 0.5), input.z);
+    }
+
+    Vector3 SnapPosition(Vector3 input, float factor = 1f)
+    {
+        if (factor <= 0f)
+            throw new UnityException("factor argument must be above 0");
+
+        float x = Mathf.Round(input.x / factor) * factor;
+        float y = Mathf.Round(input.y / factor) * factor;
+        float z = Mathf.Round(input.z / factor) * factor;
+
+        return new Vector3(x, y, z);
     }
 }
--- build-your-home/Assets/Scripts/Player/MoveController.cs
+++ build-your-home/Assets/Scripts/Player/MoveController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class MoveController : MonoBehaviour {
+public class MoveController : MonoBehaviour
+{
     public float normalSpeed;
     public float dashSpeed;
     public GameObject tileMap;
-    public Vector3 Direction {
+    public Vector3 Direction
+    {
         get;
         private set;
     }
@@ -17,22 +19,29 @@ public class MoveController : MonoBehaviour {
     private Collider2D tileMapCollider;
     new private Collider2D collider;
 
-    private float Speed {
-        get {
-            if (Input.GetButton("Dash")) {
+    private float Speed
+    {
+        get
+        {
+            if (Input.GetButton("Dash"))
+            {
                 return dashSpeed;
-            } else {
+            }
+            else
+            {
                 return normalSpeed;
             }
         }
     }
 
-    private void Start() {
+    private void Start()
+    {
         tileMapCollider = tileMap.GetComponent<Collider2D>();
         collider = GetComponent<Collider2D>();
     }
 
-    void Update () {
+    void Update()
+    {
         prevPos = transform.position;
         var horizontal = Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * Speed;
         var vertical = Input.GetAxis("Vertical") * Vector3.up * Time.deltaTime * Speed;
@@ -40,23 +49,29 @@ public class MoveController : MonoBehaviour {
         // try to move horizontally
         var currentPrevPos = prevPos;
         transform.position += horizontal;
-        if (!IsOnMap()) {
+        if (!IsOnMap())
+        {
             transform.position = prevPos;
-        } else {
+        }
+        else
+        {
             currentPrevPos = transform.position;
         }
         // try to move vertically
         transform.position += vertical;
-        if (!IsOnMap()) {
+        if (!IsOnMap())
+        {
             transform.position = currentPrevPos;
         }
-	}
+    }
 
-    private void LateUpdate() {
+    private void LateUpdate()
+    {
         // find the direction that lines up best with the change between previous and current position
         var directions = new[] { Vector3.right, Vector3.up, Vector3.left, Vector3.down };
         var delta = transform.position - prevPos;
-        if (delta.magnitude > 0) {
+        if (delta.magnitude > 0)
+        {
             // just compare dot products with cardinal directions
             Direction = directions.OrderBy(v => Vector3.Dot(v, delta))
                                   .Last();
@@ -64,10 +79,12 @@ public class MoveController : MonoBehaviour {
 
     }
 
-    private bool IsOnMap() {
-        return true;
-        // for some reason this doesn't work
-        return tileMapCollider.bounds.Contains(collider.bounds.min)
-            && tileMapCollider.bounds.Contains(collider.bounds.max);
+    private bool IsOnMap()
+    {
+        return (tileMapCollider.bounds.Contains(new Vector3(collider.bounds.min.x, collider.bounds.min.y, tileMapCollider.bounds.min.z))
+    && tileMapCollider.bounds.Contains(new Vector3(collider.bounds.max.x, collider.bounds.max.y, tileMapCollider.bounds.max.z)));
+        // bounds.contain has issues with 2D for some reason, always expecting a vector 3. Hopefully intersects is an ok replacement.
+         
+        //return !tileMapCollider.bounds.Intersects(collider.bounds);
     }
 }
