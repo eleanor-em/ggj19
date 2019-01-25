ed166360bebe7e7338fd1cbd2903c17277602b77
Holiday Course <holiday@ad.aie.edu>
2019-01-25 21:54:57 +1100

    basic movements and tileset ready

---
+++ build-your-home/Assets/Scripts/MoveController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+    public float normalSpeed;
+    public float dashSpeed;
+    public GameObject tileMap;
+
+    private Collider2D tileMapCollider;
+    new private Collider2D collider;
+
+    private float Speed {
+        get {
+            if (Input.GetButton("Dash")) {
+                return dashSpeed;
+            } else {
+                return normalSpeed;
+            }
+        }
+    }
+
+    private void Start() {
+        tileMapCollider = tileMap.GetComponent<Collider2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    void Update () {
+        var prevPos = transform.position;
+        var horizontal = Input.GetAxis("Horizontal") * Vector3.right * Time.deltaTime * Speed;
+        var vertical = Input.GetAxis("Vertical") * Vector3.up * Time.deltaTime * Speed;
+
+        // try to move horizontally
+        transform.position += horizontal;
+        if (!IsOnMap()) {
+            transform.position = prevPos;
+        } else {
+            prevPos = transform.position;
+        }
+        // try to move vertically
+        transform.position += vertical;
+        if (!IsOnMap()) {
+            transform.position = prevPos;
+        }
+	}
+
+    private bool IsOnMap() {
+        return tileMapCollider.bounds.Contains(collider.bounds.min)
+            && tileMapCollider.bounds.Contains(collider.bounds.max);
+    }
+}
