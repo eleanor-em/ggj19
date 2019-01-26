868ebdf7cf0fda18d289103f8c49b5f73088ad3e
Jimbles <jim_bob_smith@live.com.au>
2019-01-26 16:14:33 +1100

    Pushed scene so eleanor's isn't broken

---
+++ build-your-home/Assets/RiverController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverController : MonoBehaviour {
+
+    private BoxCollider2D riverCollider;
+    private Vector3 spawnPos;
+
+    // Use this for initialization
+    void Start () {
+        riverCollider = transform.GetComponent<BoxCollider2D>();
+        spawnPos = riverCollider.bounds.center + new Vector3(0, riverCollider.bounds.extents.y, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SpawnItem()
+    {
+        
+
+    }
+}
