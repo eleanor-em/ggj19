fe7c65ca4490c332ade97c3820e1dadbc04648e7
Jimbles <jim_bob_smith@live.com.au>
2019-01-26 17:56:38 +1100

    Items float away now

--- build-your-home/Assets/FloatyController.cs
+++ build-your-home/Assets/FloatyController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FloatyController : MonoBehaviour {
+    public bool shouldFloat = false;
+    BoxCollider2D myCollider;
+    BoxCollider2D river;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        myCollider = transform.GetComponent<BoxCollider2D>();
+        river = GameObject.FindGameObjectWithTag("river").GetComponent<BoxCollider2D>();
+        Interact();
 	}
-	
+
+
+    public void Interact()
+    {
+        shouldFloat = IsInRiver();
+    }
+
+    bool IsInRiver()
+    {
+        return (river.bounds.Contains(new Vector3(myCollider.bounds.min.x, myCollider.bounds.min.y))
+            && river.bounds.Contains(new Vector3(myCollider.bounds.max.x, myCollider.bounds.max.y)));
+    }
 	// Update is called once per frame
 	void Update () {
-		
+        if (shouldFloat)
+        {
+            transform.position -= new Vector3(0,1,0) * Time.deltaTime;
+
+            //if (myCollider.bounds.max.y > )
+            //{
+            //     //Should send not destroy
+            //}
+        }
 	}
 }
