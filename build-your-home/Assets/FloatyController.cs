8f828efac3807defcc5b7c62e217651831d876af
Jimbles <jim_bob_smith@live.com.au>
2019-01-26 18:58:05 +1100

    Added bobbing

--- build-your-home/Assets/FloatyController.cs
+++ build-your-home/Assets/FloatyController.cs
@@ -6,11 +6,14 @@ public class FloatyController : MonoBehaviour {
     public bool shouldFloat = false;
     BoxCollider2D myCollider;
     BoxCollider2D river;
-
+    SpriteRenderer playerSprite;
     // Use this for initialization
     void Start () {
+        oscillateRange = (endRange - startRange) / 2;
+        oscillateOffset = oscillateRange + startRange;
         myCollider = transform.GetComponent<BoxCollider2D>();
         river = GameObject.FindGameObjectWithTag("river").GetComponent<BoxCollider2D>();
+        playerSprite = transform.GetComponent<SpriteRenderer>();
         Interact();
 	}
 
@@ -31,10 +34,18 @@ public class FloatyController : MonoBehaviour {
         {
             transform.position -= new Vector3(0,1,0) * Time.deltaTime;
 
+            float offset = oscillateOffset + Mathf.Sin(Time.time * 2.5f) * oscillateRange;
+            transform.localScale = new Vector3(offset, offset, 0);
+
             if (myCollider.bounds.max.y < river.transform.GetComponent<RiverController>().endPos.y)
             {
                 Destroy(gameObject);//Should send not destroy
             }
         }
 	}
+
+    private float endRange = 0.8f;
+    private float startRange = 1.0f;
+    private float oscillateRange;
+    private float oscillateOffset;
 }
